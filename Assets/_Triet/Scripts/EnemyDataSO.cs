59d17c5cf634d93323d56b8bf99e03d8d348302c
trietdoan09 <92006097+trietdoan09@users.noreply.github.com>
2024-10-05 15:27:56 +0700

    Add large file

---
+++ Assets/_Triet/Scripts/EnemyDataSO.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy Data Manager", menuName = "Enemy/EnemyDataManager")]
+public class EnemyDataSO : ScriptableObject
+{
+    public List<EnemyData> datas = new List<EnemyData>();
+}
+
+[CreateAssetMenu(fileName = "Enemy Data", menuName = "Enemy/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public string enemyRaceName;
+    public EnemyRace race;
+    public EnemyRarity maxRarity;
+    public EnemyType maxType;
+    public float basicHp;
+    public float basicAtk;
+    public float basicAtkSpd;
+    public float basicDef;
+    public float basicCriticalRate;
+    public float basicCriticalDmg;
+    public string description;
+    public EnemySkillSO enemySkill;
+}
