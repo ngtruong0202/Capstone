fd8cf254830b0fab72d5953fcd3038d65e9193ea
ngtruong0202 <ngtruong998@gmail.com>
2024-10-31 02:22:11 +0700

    attack

--- Assets/_Truong/Scripts/Player/SwordAttack.cs
+++ Assets/_Truong/Scripts/Player/SwordAttack.cs
@@ -1,27 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SwordAttack : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private EnemyDetection enemyDetection;
 
     private int currentAttack;
     private float cooldowntime = 1;
     private float timeAttack;
+    private float enemyDistance;
+    private float quickAttackDeltaDistance;
 
-    void Start()
-    {
-        
-    }
 
-    
     void Update()
     {
         timeAttack += Time.deltaTime;
         if (Input.GetMouseButtonDown(0))
         {
-            Attack();
+            if (enemyDetection.currentTarget == null)
+                Attack();
+            else
+            {
+                MoveTowardsTarget("MMAKick",enemyDetection.currentTarget.position, quickAttackDeltaDistance);
+            }
+            
         }
     }
 
@@ -46,5 +52,44 @@ public class SwordAttack : MonoBehaviour
         }
     }
 
-  
+    public void MoveTowardsTarget(string animationName, Vector3 enemyPos, float deltaDistance)
+    {
+        PerformAttackAnimation(animationName);
+        FaceThis(enemyPos);
+
+        Vector3 finalPos = TargetOffset(enemyPos, deltaDistance);
+        finalPos.y = 0;
+
+        StartCoroutine(Move(enemyPos));
+    }
+
+    private void PerformAttackAnimation(string animationName)
+    {
+        animator.SetTrigger(animationName);
+    }
+
+    public void FaceThis(Vector3 target)
+    {
+
+        Vector3 direction = new Vector3(target.x - transform.position.x, 0, target.z - transform.position.z);
+
+        Quaternion lookAtRotation = Quaternion.LookRotation(direction);
+
+        rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, lookAtRotation, 200 * Time.deltaTime));
+    }
+
+    public Vector3 TargetOffset(Vector3 target, float deltaDistance)
+    {
+        return Vector3.MoveTowards(target, transform.position, deltaDistance);
+    }
+
+    IEnumerator Move(Vector3 targetPosition)
+    {
+        while (Vector3.Distance(rb.position, targetPosition) > 0.5f)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rb.position, targetPosition, 5 * Time.deltaTime);
+            rb.MovePosition(newPosition);
+            yield return null;
+        }
+    }
 }
